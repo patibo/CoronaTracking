b52746be3508433749581071f0ae2e335adff7ef
Maxi <58296927+Venuam1@users.noreply.github.com>
2020-12-02 11:59:45 +0100

    Update login.py

--- login.py
+++ login.py
@@ -91,3 +91,32 @@ class Login:#Hier passiert alles was im hintergrund der Webseite
 Website = Login()
 Website.neu()
 # Website.anmelden()
+
+
+#Passwort zurücksetzten
+#es wird ein generierter code als email geschickt den man dann in die app eingeben muss
+#Wichtig man kann nur einer Person eine Email schicken wenn diese Person d
+import smtplib
+
+# der empfaenger der Email
+empfaenger = input(str("Geben sie den Email-Empfaenger ein: "))
+#der Email Sender \ Email Sender passwort
+gmail__user = input(str("Geben sie ihre Email ein : "))
+gmail__pwd = input(str("Geben sie ihr Passwort ein : "))
+#Email Betreff\ Email Inhalt
+email_subject = input(str("Geben sie den Betreff ihrer Email ein : "))
+email_inhalt = input(str("Geben sie den Inhalt ihrer Email ein : "))
+
+#Es wird eine Verbindung zu smtpserver hergestellt
+smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+smtpserver.ehlo()
+smtpserver.starttls()
+smtpserver.ehlo
+smtpserver.login(gmail__user, gmail__pwd)
+header = 'To:' + empfaenger + '\n' + 'From: ' + gmail__user + '\n' + "Subject:" + email_subject + "\n"
+msg = header + "\n " + email_inhalt + " \n\n"
+smtpserver.sendmail(gmail__user, empfaenger, msg)
+print (header)
+print ('done!')
+#Die verbindung wird geschlossen
+smtpserver.close()
