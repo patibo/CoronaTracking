e6c2d6bf55f5e17d2794901b5c90deeaab7a0619
RiegerAlisa00 <62987663+RiegerAlisa00@users.noreply.github.com>
2020-12-02 15:50:56 +0100

    verlauf, email

--- login.py
+++ login.py
@@ -1,9 +1,7 @@
 import re #Bibliothek regex wird importiert. Ist für die Überprüfung der Eingaben.
 import mysql.connector#Bibliothek mysql wird importiert. Ist für die Verbindung zur Datenbank.
 import datetime#Bibliothek datetime wird importiert. Ist für die Prüfung vom Geburtsdatum.
-
-#Alisa Rieger Code
-
+import smtplib
 class DB:#Hier passiert alles was mit der DB zutun hat
     def __init__(self,user,pswd,host,port,db):#Hier werden die Informationen übergeben die ich für eine DB verbindung brauche
         self.config = {
@@ -18,12 +16,20 @@ class DB:#Hier passiert alles was mit der DB zutun hat
     def connect(self):#eine Verbindung zur DB wird erstellt
         self.mydb = mysql.connector.connect(**self.config)
         self.cur = self.mydb.cursor()
-    def insert(self,benutzername,pswt,vorname,nachname,email,geburtsdatum,telefon):#ein Neuer Kunde wird erstellt
+    def insert_kunde(self,benutzername,pswt,vorname,nachname,email,geburtsdatum,telefon):#ein Neuer Kunde wird erstellt
         sql = "INSERT INTO `kunden`(`benutzername`, `passwort`, `vorname`, `nachname`, `email`, `geburtsdatum`, `telefon`) VALUES ('{}','{}','{}','{}','{}','{}','{}')".format(benutzername,pswt,vorname,nachname,email,geburtsdatum,telefon)
         self.cur.execute(sql)
         self.mydb.commit()
-    def select(self,benutzername_email):#Hier wird das Passwort zurückgegeben was ich für die Anmeldung brauche
+    def insert_kundenevent(self,kunden_id,event_id):
+        sql="INSERT INTO `kundenevents` (`kID`, `eID`) VALUES ('{}', '{}');".format(kunden_id,event_id)
+        self.cur.execute(sql)
+        self.mydb.commit()
+    def select_pswd(self,benutzername_email):#Hier wird das Passwort zurückgegeben was ich für die Anmeldung brauche
         sql = "SELECT passwort FROM `kunden` WHERE benutzername = '{}' or email = '{}'".format(benutzername_email, benutzername_email)
+        self.cur.execute(sql)
+        return self.cur.fetchall()
+    def select_id(self,benutzername_email,pswd):
+        sql = "SELECT id FROM kunden WHERE (`benutzername`='{}' or `email`='{}') and `passwort` = '{}' ".format(benutzername_email,pswd)
         self.cur.execute(sql)
         return self.cur.fetchall()
     def close(self):#die Verbindung zur DB wird getrennt
@@ -33,13 +39,14 @@ class DB:#Hier passiert alles was mit der DB zutun hat
 class Login:#Hier passiert alles was im hintergrund der Webseite
     def __init__(self):
         self.db = DB('root','root','localhost','8889','coronatracking')#hier kann ich die Klasse DB verwenden bzw. hier wird sie aufgerufen
+        self.id = None
     def anmelden(self):#anmelde funktion
         benutzername_email = 'ar'#Eingabefeld für Benutzername bzw. E-Mail
         pswt = 'a'#Eingabefeld für das Passwort
         
         benutzername_email = benutzername_email.strip()#Leerzeichen werden am Anfang und am Ende entfernt
         self.db.connect()#Verbindung zur DB wird erstellt
-        pswd = self.db.select(benutzername_email)#das Passwort was zum Benutzername/E-Mail passt was in der DB gespeichert wurde wird hier zurückgegeben
+        pswd = self.db.select_pswd(benutzername_email)#das Passwort was zum Benutzername/E-Mail passt was in der DB gespeichert wurde wird hier zurückgegeben
 
         if pswt != pswd[0][0]:#hier wird geschaut ob das Passwort im Eingabefeld das gleiche Passwort ist wie in der DB. Stimmt es nicht kommt eine Fehlermeldung.
             fehler_medlung = "Fehler! Benutzername/E-Mail oder Passwort stimmt nicht."
@@ -85,38 +92,34 @@ class Login:#Hier passiert alles was im hintergrund der Webseite
 
 
         self.db.connect()#verbindung zur DB wird erstellt
-        # self.db.insert(benutzername,pswt,vorname,nachname,email,geburtsdatum,telefon)#neues Konot wird erstellt
+        self.db.insert_kunde(benutzername,pswt,vorname,nachname,email,geburtsdatum,telefon)#neues Konot wird erstellt
         self.db.close()#Verbindung zur DB wird getrentt
+    def kunde_id(self,benutzername_email,pswt):
+        self.db.connect()#Verbindung zur DB wird erstellt
+        self.id = self.db.select_id(benutzername_email,pswt)
+        self.id = self.id[0][0]
+        self.db.close()#Verbindung zur DB wird getrent
     
+    def kundenevent(self):
+        self.db.connect()#Verbindung zur DB wird erstellt
+        event_id = '1'
+        self.db.insert_kundenevent(self.id,event_id)
+        self.db.close()#Verbindung zur DB wird getrent
+    def mail(self):
+        user = 'alisa6rieger@gmail.com'
+        pwd = 'spuzunwyyivbpnbe'
+        mail_text = 'Es gab einen Coronafall!'
+        subject = 'Warnung!'
+
+        
+        MAIL_FROM = user
+        RCPT_TO  = ", ".join(emails)
+        DATA = 'From:%s\nTo:%s\nSubject:%s\n\n%s' % (MAIL_FROM, RCPT_TO, subject, mail_text)
+        server = smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(MAIL_FROM, RCPT_TO, DATA)
+        server.quit()
 Website = Login()
 Website.neu()
 # Website.anmelden()
-
-
-#Passwort zurücksetzten
-#es wird ein generierter code als email geschickt den man dann in die app eingeben muss
-#Wichtig man kann nur einer Person eine Email schicken wenn diese Person d
-import smtplib
-
-# der empfaenger der Email
-empfaenger = input(str("Geben sie den Email-Empfaenger ein: "))
-#der Email Sender \ Email Sender passwort
-gmail__user = input(str("Geben sie ihre Email ein : "))
-gmail__pwd = input(str("Geben sie ihr Passwort ein : "))
-#Email Betreff\ Email Inhalt
-email_subject = input(str("Geben sie den Betreff ihrer Email ein : "))
-email_inhalt = input(str("Geben sie den Inhalt ihrer Email ein : "))
-
-#Es wird eine Verbindung zu smtpserver hergestellt
-smtpserver = smtplib.SMTP("smtp.gmail.com",587)
-smtpserver.ehlo()
-smtpserver.starttls()
-smtpserver.ehlo
-smtpserver.login(gmail__user, gmail__pwd)
-header = 'To:' + empfaenger + '\n' + 'From: ' + gmail__user + '\n' + "Subject:" + email_subject + "\n"
-msg = header + "\n " + email_inhalt + " \n\n"
-smtpserver.sendmail(gmail__user, empfaenger, msg)
-print (header)
-print ('done!')
-#Die verbindung wird geschlossen
-smtpserver.close()
