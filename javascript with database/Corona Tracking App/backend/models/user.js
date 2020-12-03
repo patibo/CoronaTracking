140ba63bd8f0906451dd332d555103d6bb561e29
ilirjan1998 <52413531+ilirjan1998@users.noreply.github.com>
2020-12-04 00:48:23 +0100

    create user and login user

---
+++ javascript with database/Corona Tracking App/backend/models/user.js
@@ -0,0 +1,31 @@
+const db = require('../util/database');
+
+module.exports = class User{
+    constructor(id,email,password){
+        this.id=id;
+        this.email=email;
+        this.password=password;
+        this.role="user";
+    }
+
+    static find(user){
+        return db.execute('select * from user where email = ? and password = ?',[user.email,user.password]);
+    }
+    static fetchAll() {
+        return db.execute('SELECT * FROM user');
+    }
+
+    static post(user) {
+        // console.log(email);
+        // console.log(password);
+        return db.execute('Insert into user (email, password, role) Values (?,?,?)', [user.email,user.password,user.role]);
+    }
+
+    static update(user){
+        return db.execute('update user set email = ? , password = ?, role = ? where id = ?', [user.email,user.password,user.role, user.id]);
+    }
+
+    static delete(id){
+        return db.execute('delete from user where id = ?', [id]);
+    }
+};
