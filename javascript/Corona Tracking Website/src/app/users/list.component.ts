344c41dee5f36b31b806d4b75f33883992cc6ba0
ilirjan1998 <52413531+ilirjan1998@users.noreply.github.com>
2020-12-03 14:12:13 +0100

    corona tracking

--- javascript/Corona Tracking Website/src/app/users/list.component.ts
+++ javascript/Corona Tracking Website/src/app/users/list.component.ts
@@ -3,7 +3,9 @@ import { first } from 'rxjs/operators';
 
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'list.component.html' })
+@Component({ templateUrl: 'list.component.html',
+  styleUrls: ['list.component.css']
+})
 export class ListComponent implements OnInit {
     users = null;
 
