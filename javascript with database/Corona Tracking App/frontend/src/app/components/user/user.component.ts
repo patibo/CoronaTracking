640330aabc2153ecd102ee6bb53fffca93cc4e15
ilirjan1998 <52413531+ilirjan1998@users.noreply.github.com>
2020-12-04 01:16:58 +0100

    create user and login user

--- javascript with database/Corona Tracking App/frontend/src/app/components/user/user.component.ts
+++ javascript with database/Corona Tracking App/frontend/src/app/components/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserListCrudService } from 'src/app/services/user-list-crud.service';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/User';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -10,66 +10,66 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  loggedInUser$:User;
+  constructor(private userListCrudService: UserListCrudService, private router: Router) { }
+  loggedInUser$: User;
   updateUserForm: FormGroup;
-  constructor(private userListCrudService:UserListCrudService,private router: Router) { }
+
+  showDetails = false;
 
   ngOnInit(): void {
-    this.loggedInUser$=JSON.parse(sessionStorage.getItem('currentUser'));
+    this.loggedInUser$ = JSON.parse(sessionStorage.getItem('currentUser'));
     this.updateUserForm = this.createFormGroup();
     this.userName();
   }
 
-  createFormGroup():FormGroup{
+  createFormGroup(): FormGroup{
     return new FormGroup({
-      id: new FormControl(this.loggedInUser$.id,[Validators.required]),
-      email: new FormControl(this.loggedInUser$.email,[Validators.required]),
-      password: new FormControl(this.loggedInUser$.password,[Validators.required]),
-      role: new FormControl(this.loggedInUser$.role,[Validators.required])
+      id: new FormControl(this.loggedInUser$.id, [Validators.required]),
+      email: new FormControl(this.loggedInUser$.email, [Validators.required]),
+      password: new FormControl(this.loggedInUser$.password, [Validators.required]),
+      role: new FormControl(this.loggedInUser$.role, [Validators.required])
     });
   }
 
-  deleteSessionUserInfo():void{
+  deleteSessionUserInfo(): void{
     sessionStorage.removeItem('currentUser');
   }
 
-  displayPerson():void{
+  displayPerson(): void{
     console.log(this.loggedInUser$);
-    let myContainer = document.getElementById('last') as HTMLElement;
-    myContainer.innerHTML = "hello";
+    const myContainer = document.getElementById('last') as HTMLElement;
+    myContainer.innerHTML = 'hello';
   }
 
-  userName():void{
+  userName(): void{
     console.log(this.loggedInUser$);
-    let myContainer2 = document.getElementById('hiName') as HTMLElement;
-    myContainer2.innerHTML = "Hello <b>" + this.loggedInUser$.email + "</b> !!!";
+    const myContainer2 = document.getElementById('hiName') as HTMLElement;
+    myContainer2.innerHTML = 'Hello <b>' + this.loggedInUser$.email + '</b> !!!';
   }
-  
 
-  delete():void{
+
+  delete(): void{
     this.userListCrudService.delete(this.loggedInUser$.id).subscribe();
     sessionStorage.removeItem('currentUser');
-    this.router.navigate([""]);
+    this.router.navigate(['']);
   }
-  update():void{
+  update(): void{
     console.log(this.updateUserForm.value);
     // this.userListCrudService.update(this.loggedInUser$.id).subscribe();
     // sessionStorage.removeItem('currentUser');
     // this.router.navigate([""]);
     this.userListCrudService.update(this.updateUserForm.value).subscribe();
-    sessionStorage.setItem('currentUser',JSON.stringify(this.updateUserForm.value));
+    sessionStorage.setItem('currentUser', JSON.stringify(this.updateUserForm.value));
    // window.location.reload();
   }
-
-  showDetails:boolean=false;
-  showUserDetailfunc():void{
-    if(this.showDetails==false){
+  showUserDetailfunc(): void{
+    if (this.showDetails === false){
       this.showDetails = true;
     }
     else{
-      this.showDetails=false;
+      this.showDetails = false;
     }
   }
 
-  
+
 }
