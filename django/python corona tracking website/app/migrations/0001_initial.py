95dc6a2bdb4ff1927b553f057dfa1085be88ca8a
ilirjan1998 <52413531+ilirjan1998@users.noreply.github.com>
2020-12-10 09:44:52 +0100

    website

--- django/python corona tracking website/app/migrations/0001_initial.py
+++ django/python corona tracking website/app/migrations/0001_initial.py
@@ -20,10 +20,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True,
                                         serialize=False, verbose_name='ID')),
                 ('birthday', models.DateField(blank=True)),
-                ('friend_requests', models.ManyToManyField(blank=True, related_name='friend_requests',
-                                                           to=settings.AUTH_USER_MODEL)),
-                ('friends', models.ManyToManyField(blank=True, related_name='friends',
-                                                   to=settings.AUTH_USER_MODEL)),
+
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
                                            related_name='profile', to=settings.AUTH_USER_MODEL)),
             ],
